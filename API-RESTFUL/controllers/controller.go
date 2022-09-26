41566020eaf12192ed395afc46e059bbb6f08561
Ivo Aguiar Pimenta <ivoaguiarpimenta@gmail.com>
2022-09-26 11:47:37 -0300

    Implementation of models and controllers

---
+++ API-RESTFUL/controllers/controller.go
@@ -0,0 +1,30 @@
+package controllers
+
+import (
+	"API-RESTFUL/database"
+	"API-RESTFUL/models"
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func Home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Home page")
+}
+
+func TodasPersonalidades(w http.ResponseWriter, r *http.Request) {
+	var p []models.Personalidade
+	database.DB.Find(&p)
+	json.NewEncoder(w).Encode(p)
+}
+
+func RetornaPersonalidades(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+	var personalidade models.Personalidade
+	database.DB.First(&personalidade, id)
+	json.NewEncoder(w).Encode(personalidade)
+
+}
